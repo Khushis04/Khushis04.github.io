405e314e25f08fbd46c44f2dce010f5a20aa1166
Khushis04 <127972471+Khushis04@users.noreply.github.com>
2025-11-02 00:06:20 +0530

    Create About.jsx

---
+++ client/src/components/About.jsx
@@ -0,0 +1,13 @@
+export default function About() {
+  return (
+    <section id="about">
+      <h2>About Me</h2>
+      <p>
+        My passion for coding comes from the ability to design how things work
+        behind the scenes. I enjoy breaking down complex problems, building
+        individual components, and then connecting them into a system where
+        everything works together seamlessly.
+      </p>
+    </section>
+  );
+}
