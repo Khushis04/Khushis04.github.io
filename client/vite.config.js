b88ad8f57829defe91542dd151bf04b5261bdb04
Khushis04 <127972471+Khushis04@users.noreply.github.com>
2025-11-02 00:24:18 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // keep "/" because your repo is Khushis04.github.io (special case)
+  base: "/",
 });
