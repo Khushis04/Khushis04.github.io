502fe99a70cce21853ec844eaa0b26653c2b6a5d
Khushis04 <127972471+Khushis04@users.noreply.github.com>
2025-11-02 00:07:24 +0530

    Create Contact.jsx

---
+++ client/src/components/Contact.jsx
@@ -0,0 +1,12 @@
+export default function Contact() {
+  return (
+    <section id="contact">
+      <h2>Contact</h2>
+      <p>Email: khushissingh.04@email.com</p>
+      <p>
+        <a href="https://github.com/Khushis04">GitHub</a> | 
+        <a href="https://linkedin.com/in/khushi-singh-0b12b726a"> LinkedIn</a>
+      </p>
+    </section>
+  );
+}
