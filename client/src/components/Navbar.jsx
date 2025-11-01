f0bc8ad2daa9c5e65748f48242cf4e283acd98aa
Khushis04 <127972471+Khushis04@users.noreply.github.com>
2025-11-02 00:04:49 +0530

    Create Navbar.jsx

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <h1>Khushi Singh</h1>
+      <ul>
+        <li><a href="#about">About</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
